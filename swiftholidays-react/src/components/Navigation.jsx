fe002489089a99e8799fa65f0fba4309ba7133c0
surakshya0804 <126639389+surakshya0804@users.noreply.github.com>
2024-05-07 12:20:50 +0545

    hero section and navigation part editing

---
+++ swiftholidays-react/src/components/Navigation.jsx
@@ -0,0 +1,22 @@
+const Navigation = () => {
+    return (
+        <div className="">
+            <nav className=' flex-1 bg-zinc-200  sticky ' >
+            <div className="logo ">
+                <img className='w-36 ml-8 ' src="src/swiftholidayslogo.png" alt="logo" />
+            </div>
+
+            <ul className='flex gap-14 justify-end mr-6 '>
+                <li>Home </li>
+                <li> Company Profile</li>
+                <li>Our Service </li>
+                <li> Bus</li>
+                <li> Contant Us</li>
+
+            </ul>
+
+        </nav>
+        </div>
+    )
+}
+export  default Navigation;
