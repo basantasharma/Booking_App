4497dc484f5d8c76c98c75bb1983e693fd2730e0
prakriti92 <prakritiadhakari58@gmail.com>
2024-05-12 12:16:56 +0545

    prakriti changes

---
+++ swiftholidays-react/src/components/ContactUsPages/Contactpage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Contactentry from "../HomePages/Contactentry";
+
+const Contactpage = () => {
+  //const name = "prakriti";
+  return (
+    <div>
+      <Contactentry />
+      {/* name={name} /> */}
+    </div>
+  );
+};
+
+export default Contactpage;
